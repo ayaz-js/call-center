ee8536438467c0e46a745a3b618c71eced671eeb
ayaz-js <jetheez@yandex.ru>
2021-11-27 17:29:47 +0200

    add slider and blocks

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
+// Redner Service Items
 function serviceItemsCreate() {
     const serviceItemsBlock = document.querySelector('.service__items');
+    // Service Items Collection
     const serviceItems = [{
             serviceIcon: `img/1.svg`,
             serviceTitle: `Marketing & SEO Agency`,
@@ -32,7 +34,6 @@ function serviceItemsCreate() {
         },
     ];
 
-
     const serviceItemsTemplate = ({
         serviceIcon,
         serviceTitle,
@@ -61,3 +62,79 @@ function serviceItemsCreate() {
 }
 
 serviceItemsCreate();
+// Render Sevice Items End
+
+// Render Partner Logo
+function sliderCreate() {
+    const sliderBlock = document.querySelector('.our-partners__slider-wrapper');
+
+    // Partner Logo Collection
+    const sliderItems = [{
+            sliderImg: 'img/logo-1.png'
+        },
+        {
+            sliderImg: 'img/logo-2.png'
+        },
+        {
+            sliderImg: 'img/logo-3.png'
+        },
+        {
+            sliderImg: 'img/logo-4.png'
+        },
+        {
+            sliderImg: 'img/logo-5.png'
+        },
+        {
+            sliderImg: 'img/logo-6.png'
+        },
+    ];
+
+    const sliderTemplate = ({
+        sliderImg
+    }) => {
+        return `
+            <div class="our-partners__img swiper-slide">
+                <img src="${sliderImg}" alt="">
+            </div>
+        `;
+    }
+
+    let sliderItemsTemplateString = '';
+    sliderItems.forEach(items => {
+        sliderItemsTemplateString += sliderTemplate(items)
+    })
+
+    sliderBlock.innerHTML = sliderItemsTemplateString;
+
+
+    var swiper = new Swiper('.our-partners__slider', {
+        slidesPerView: 1,
+        spaceBetween: 10,
+        loop: true,
+        autoplay: true,
+        pagination: {
+            clickable: true,
+        },
+        breakpoints: {
+            425: {
+                slidesPerView: 2,
+                spaceBetween: 20,
+            },
+            640: {
+                slidesPerView: 2,
+                spaceBetween: 20,
+            },
+            768: {
+                slidesPerView: 3,
+                spaceBetween: 40,
+            },
+            1024: {
+                slidesPerView: 5,
+                spaceBetween: 50,
+            },
+        },
+    });
+}
+
+sliderCreate();
+// Render Partner Logo End
